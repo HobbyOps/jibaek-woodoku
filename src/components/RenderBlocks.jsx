c2db80876ce886d5b56e259b5b5a6c9f9b639da5
baekjaein <baek2506@gmail.com>
2025-07-01 22:53:55 +0900

    Commit new "R"enderBlock.jsx

---
+++ src/components/RenderBlocks.jsx
@@ -0,0 +1,42 @@
+import RenderBlockRow from "./RenderBlockRow";
+
+function RenderBlocks({blockCode}) {
+
+    const defBlocks = {
+        0: {
+
+        },
+        1: {
+            1: [1, 1, 1, 1, 1]
+        },
+        2: {
+            1: [1, 1, 1]
+        },
+        3: {
+            1: [1],
+            2: [1],
+            3: [1],
+            4: [1],
+            5: [1]
+        },
+        4: {
+            1: [1],
+            2: [1],
+            3: [1]
+        },
+        5: {
+            1: [1, 1],
+            2: [1, 1],
+        }
+    }
+    
+    const blockRowMap = Object.values(defBlocks[blockCode]).map((row, index) => (<RenderBlockRow value={row} key={index}></RenderBlockRow>))
+    
+    return (
+        <div className="block-wrap">
+            {blockRowMap}
+        </div>
+    );
+}
+
+export default RenderBlocks;
